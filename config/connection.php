85655a8c3a995d4972615cc4dc150887de1d1b51
kank3y <anyc.nft@gmail.com>
2024-06-16 19:40:50 +0800

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,13 +1,19 @@
 <?php
-    require 'constants.php';
+require_once __DIR__ . '/../config/constants.php';
 
-    // Create connection
-    $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+// Initialize connection
+$connection = mysqli_init();
 
-    // Check connection
-    if (!$connection) {
-        die("Connection failed: " . mysqli_connect_error());
-    } else {
-        echo "Connected successfully";
-    }
+// Set SSL options if needed
+mysqli_ssl_set($connection, NULL, NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+// Attempt to connect to the database
+mysqli_real_connect($connection, "calorietracker.mysql.database.azure.com", "calorietracker", "!ctracker1234", "ctrackerdb", 3306, MYSQLI_CLIENT_SSL);
+
+// Check if connection was successful
+if (mysqli_connect_errno()) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
+}
 ?>
