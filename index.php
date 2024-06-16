e5f7082adbcc7ae523592ddf2af21d0dad5f0593
kank3y <anyc.nft@gmail.com>
2024-06-16 19:33:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
     session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
     $page_title = 'Login Page';
     include ('includes/header_index.html');
     require_once 'config/connection.php';
