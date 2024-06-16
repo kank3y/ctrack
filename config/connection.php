75a518588236ce24841907577ef2763ff1063924
kank3y <anyc.nft@gmail.com>
2024-06-16 19:59:59 +0800

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../config/constants.php';
 // Create connection
 $connection = mysqli_init();
 mysqli_ssl_set($connection, NULL, NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($connection, "calorietracker.mysql.database.azure.com", "calorietracker", "!ctracker1234", "ctrackerdb", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($connection, "calorietracker.mysql.database.azure.com", "calorietracker", "!ctracker1234", "ctrackerdb", 3306);
 
 // Check connection
 if (!$connection) {
