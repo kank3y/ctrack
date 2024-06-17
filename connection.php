f63d54728a15bd3cf0710e2a38f3c3b0bf05d94c
kank3y <anyc.nft@gmail.com>
2024-06-17 12:39:20 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,19 @@
-<?php
+// Create a MySQLi object instance
 $connection = mysqli_init();
-mysqli_ssl_set($connection,NULL,NULL, "/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($connection, "calorietracker.mysql.database.azure.com", "calorietracker", "!ctracker1234", "ctrackerdb", 3306, MYSQLI_CLIENT_SSL);
-?>
+
+// Set SSL options
+if (!$connection) {
+    die('mysqli_init failed');
+}
+
+if (!mysqli_ssl_set($connection, NULL, NULL, __DIR__ . '/DigiCertGlobalRootCA.crt.pem', NULL, NULL)) {
+    die('Setting SSL failed');
+}
+
+// Connect to the database
+if (!mysqli_real_connect($connection, "calorietracker.mysql.database.azure.com", "calorietracker", "!ctracker1234", "ctrackerdb", 3306, MYSQLI_CLIENT_SSL)) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+
+// Connection successful
+echo 'Connected successfully';
