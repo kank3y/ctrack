d8e0dcc2992577a11aa0237c9948cc91bb8f8562
kank3y <anyc.nft@gmail.com>
2024-06-17 12:08:45 +0800

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ctrackerdb";
+
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+?>
---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php
+	$page_title = 'Delete Page';
+	include ('header.html');
+	require 'connection.php';
+
+
+	// Get the id of the record to delete
+$id = mysqli_real_escape_string($connection, $_GET['id']);
+
+// Execute the query to delete the record
+$query = "DELETE FROM calc_table WHERE id = '$id'";
+$result = mysqli_query($connection, $query);
+
+// Check if query is successful
+if(!$result) {
+  die('Query failed ' . mysqli_error($connection));
+}
+
+// Redirect the user back to the page that displays all the records
+header("Location: calculator.php");
+exit();
+?>
