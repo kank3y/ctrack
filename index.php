a9eed91bb83adc56e7b2daee4a57c23d7f6b5cd2
kank3y <anyc.nft@gmail.com>
2024-06-17 13:11:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
- require 'connection.php';
-session_start();
- 
-    
-
-    // Check if the user is already logged in, if yes then redirect him to welcome page
+ session_start();
+require 'connection.php';
+ // Check if the user is already logged in, if yes then redirect him to welcome page
     if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
         header("location: calculator.php");
         exit;
