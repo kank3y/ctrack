3bc4179409b251a1b15fe7bcf6eea7abca29f65b
kank3y <anyc.nft@gmail.com>
2024-06-17 13:56:29 +0800

    Delete Calorie Tracker directory

--- Calorie Tracker/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$connection = mysqli_init();
-mysqli_ssl_set($connection, NULL, NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($connection, "calorietracker.mysql.database.azure.com", "calorietracker", "!ctracker1234", "ctrackerdb", 3306, MYSQLI_CLIENT_SSL);
-?>
