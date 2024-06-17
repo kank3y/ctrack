d408468db2cb1964c73bdd55f8d74cf8719fef36
kank3y <anyc.nft@gmail.com>
2024-06-17 12:57:45 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,20 +7,21 @@ if (!$connection) {
     die('mysqli_init failed');
 }
 
-// Replace 'DigiCertGlobalRootCA.crt.pem' with the correct path to your certificate file
+// Path to your certificate file (assuming it's in the same directory as this script)
 $cert_file = __DIR__ . '/DigiCertGlobalRootCA.crt.pem';
 
 if (!mysqli_ssl_set($connection, NULL, NULL, $cert_file, NULL, NULL)) {
     die('Setting SSL failed');
 }
 
-// Connect to the database
+// Database connection details
 $servername = "calorietracker.mysql.database.azure.com";
 $username = "calorietracker";
 $password = "!ctracker1234";
 $dbname = "ctrackerdb";
 $port = 3306;
 
+// Connect to the database
 if (!mysqli_real_connect($connection, $servername, $username, $password, $dbname, $port, MYSQLI_CLIENT_SSL)) {
     die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
 }
@@ -29,6 +30,12 @@ if (!mysqli_real_connect($connection, $servername, $username, $password, $dbname
 echo 'Connected successfully';
 
 // Perform your database operations here using $connection
+// For example:
+// $query = "SELECT * FROM your_table";
+// $result = mysqli_query($connection, $query);
+// while ($row = mysqli_fetch_assoc($result)) {
+//     // Process each row
+// }
 
 // Close the connection
 mysqli_close($connection);
