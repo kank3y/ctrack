8007894b4dff74d52c9b870c33ddb01c1d9c3887
kank3y <anyc.nft@gmail.com>
2024-06-17 13:07:27 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,35 @@
 <?php
+// Create a MySQLi object instance
 $connection = mysqli_init();
-mysqli_ssl_set($connection,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($connection, "calorietracker.mysql.database.azure.com", "calorietracker", "!ctracker1234", "ctrackerdb", 3306, MYSQLI_CLIENT_SSL);
+
+// Set SSL options
+if (!$connection) {
+    die('mysqli_init failed');
+}
+
+// Replace 'DigiCertGlobalRootCA.crt.pem' with the correct path to your certificate file
+$cert_file = __DIR__ . '/DigiCertGlobalRootCA.crt.pem';
+
+if (!mysqli_ssl_set($connection, NULL, NULL, $cert_file, NULL, NULL)) {
+    die('Setting SSL failed');
+}
+
+// Connect to the database
+$servername = "calorietracker.mysql.database.azure.com";
+$username = "calorietracker";
+$password = "!ctracker1234";
+$dbname = "ctrackerdb";
+$port = 3306;
+
+if (!mysqli_real_connect($connection, $servername, $username, $password, $dbname, $port, MYSQLI_CLIENT_SSL)) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+
+// Connection successful
+echo 'Connected successfully';
+
+// Perform your database operations here using $connection
+
+// Close the connection
+mysqli_close($connection);
 ?>
