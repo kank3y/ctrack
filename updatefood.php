5f583a94876333d7df543429d48d242f28664c5a
kank3y <anyc.nft@gmail.com>
2024-06-24 16:09:55 +0800

    Update updatefood.php

--- updatefood.php
+++ updatefood.php
@@ -3,6 +3,7 @@ session_start();
 require 'connection.php';
 
 $page_title = 'Update Page';
+include ('header.html');
 
 
 // Check if the user is logged in, if not then redirect him to the login page
