647fe4d006f40cdf1e0101ebfc6e5ee5601c8f82
kank3y <anyc.nft@gmail.com>
2024-06-24 15:18:16 +0800

    Update updatefood.php

--- updatefood.php
+++ updatefood.php
@@ -2,10 +2,6 @@
 session_start();
 require 'connection.php';
 
-// Enable error reporting for debugging purposes
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $page_title = 'Update Page';
 include ('header.html');
 
@@ -47,35 +43,39 @@ if (isset($_POST['updateBtn'])) {
         die('Query failed ' . mysqli_error($connection));
     }
 
+    // Close the connection
+    mysqli_close($connection);
+
     // Redirect the user back to the page that displays all the records
     header("Location: calculator.php");
     exit();
 }
-
-// Close the connection
-mysqli_close($connection);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="author" content="Group 4">
     <meta name="keywords" content="HTML, CSS, JavaScript">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>    
+
     <link rel="stylesheet" href="css/designss.css">
+
     <style>
         body {
             background-image: url('images/foodbg.png');
-            background-repeat: no-repeat;
-            background-attachment: fixed;
+            background-repeat: no-repeat;background-attachment: fixed;
             background-size: cover;
         }
     </style>
 </head>
 <body>
+
     <br>
+
     <div class="ufooddatatable">
         <h1>Update Food Data</h1>
         <hr>
@@ -106,5 +106,6 @@ mysqli_close($connection);
     </div>
 
     <?php include ('footer.html'); ?>
+
 </body>
 </html>
